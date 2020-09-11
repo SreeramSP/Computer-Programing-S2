efaf22bd602a7365b30a92c47ee4d2d38c64f84d
Sreeram SP <45159343+SreeramSP@users.noreply.github.com>
2020-09-11 11:02:26 +0530

    Add files via upload

---
+++ Difference Of 2 Numbers.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Subraction Of 2 Numbers
+By Sreeram SP 
+
+*******************************************************************************/
+
+
+#include<stdio.h>
+int main()
+{
+   int a,b,c;
+   
+    printf("Enter a = ");
+    scanf("%d",&a);
+    printf("Enter b = ");
+    scanf("%d",&b);
+    c = a - b;
+    printf("Differnce of a and b = %d\n",c);
+    return 0;
+}
---
+++ Sum Of 2 Nmbers.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Sum Of 2 Numbers
+By Sreeram SP 
+
+*******************************************************************************/
+
+
+#include<stdio.h>
+int main()
+{
+   int a,b,c;
+   
+    printf("Enter a = ");
+    scanf("%d",&a);
+    printf("Enter b = ");
+    scanf("%d",&b);
+    c = a + b;
+    printf("Sum of a and b = %d\n",c);
+    return 0;
+}
