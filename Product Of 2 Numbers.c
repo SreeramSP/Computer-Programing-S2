ea21603178f905c7aa1a47e825fb0d3544720acb
Sreeram SP <45159343+SreeramSP@users.noreply.github.com>
2020-09-13 09:49:40 +0530

    Add files via upload

---
+++ Product Of 2 Numbers.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Product Of 2 Numbers
+By Sreeram SP 
+
+*******************************************************************************/
+
+
+#include<stdio.h>
+int main()
+{
+   int a,b,c;
+   
+    printf("Enter a = ");
+    scanf("%d",&a);
+    printf("Enter b = ");
+    scanf("%d",&b);
+    c = a * b;
+    printf("Product of a and b = %d\n",c);
+    return 0;
+}
